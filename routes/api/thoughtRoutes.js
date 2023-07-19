4c6c24f33a944658633c12ced632392c8cca5c31
KGBerry542 <74325710+KGBerry542@users.noreply.github.com>
2023-07-18 20:09:12 -0400

    implementing api routes for thoughts

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const Thought = require('../../models/Thought');
+
+// GET all thoughts
+router.get('/', async (req, res) => {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch thoughts' });
+  }
+});
+
+// GET a thought by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.id);
+    if (!thought) {
+      return res.status(404).json({ error: 'Thought not found' });
+    }
+    res.json(thought);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch thought' });
+  }
+});
+
+// POST a new thought
+router.post('/', async (req, res) => {
+  try {
+    const newThought = await Thought.create(req.body);
+    res.status(201).json(newThought);
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to create thought' });
+  }
+});
+
+// PUT/update a thought by ID
+router.put('/:id', async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!thought) {
+      return res.status(404).json({ error: 'Thought not found' });
+    }
+    res.json(thought);
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to update thought' });
+  }
+});
+
+// DELETE a thought by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndDelete(req.params.id);
+    if (!thought) {
+      return res.status(404).json({ error: 'Thought not found' });
+    }
+    res.json({ message: 'Thought deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to delete thought' });
+  }
+});
+
+module.exports = router;
